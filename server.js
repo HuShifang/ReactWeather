646445c6dfd3873db124eeb4e3300fa4e8944510
HuShifang <stephen.p.ford@gmail.com>
2016-07-07 11:21:09 -0700

    s6l46 Deploying Your App -- Updated package.json, server.js with fix -- using req.protocol rather than video's outdated req.headers('x-forwarded-proto')

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
-  if (req.headers('x-forwarded-proto') === 'http') {
+  if (req.protocol === 'http') {
     next();
   } else {
     res.redirect('http://' + req.hostname + req.url);
   }
-  
+
 });
 
 app.use(express.static('public'));
