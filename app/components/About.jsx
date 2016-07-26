b19bd7d0768474e88b74742793cb4a58c4496378
HuShifang <stephen.p.ford@gmail.com>
2016-07-26 10:41:43 -0700

    added custom CSS and app/styles/ folder

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var {Link} = require('react-router');
 var About = function (props) {
   return (
     <div>
-      <h1 className='text-center'>About</h1>
+      <h1 className='text-center page-title'>About</h1>
       <p>Welcome to the About page.</p>
       <p>This app uses weather information from <a href='http://openweathermap.org/'>OpenWeatherMap</a>.
       </p>
